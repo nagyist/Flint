23354dcde66d42cfa0bc34d21dafd249d8d78ea7
Riley Testut <riley@rileytestut.com>
2016-03-07 12:52:13 -0800

    Initial Commit

---
+++ Flint/Flint.h
@@ -0,0 +1,19 @@
+//
+//  Flint.h
+//  Flint
+//
+//  Created by Riley Testut on 3/7/16.
+//  Copyright © 2016 Spark SC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Flint.
+FOUNDATION_EXPORT double FlintVersionNumber;
+
+//! Project version string for Flint.
+FOUNDATION_EXPORT const unsigned char FlintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flint/PublicHeader.h>
+
+
