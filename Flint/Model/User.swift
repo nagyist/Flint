08efdd85482f72b8bc6051332bf2351d5d3c05fe
Riley Testut <riley@rileytestut.com>
2016-04-04 14:50:12 -0700

    Adds mogenerator for easier generation of Core Data models

---
+++ Flint/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Flint
+//
+//  Created by Spark SC on 4/4/16.
+//  Copyright (c) 2016 Spark SC. All rights reserved.
+//
+
+import Foundation
+
+@objc(User)
+public class User: _User 
+{
+	// Custom logic goes here.
+}
---
+++ Flint/Model/_User.swift
@@ -0,0 +1,37 @@
+// DO NOT EDIT. This file is machine-generated and constantly overwritten.
+// Make changes to User.swift instead.
+
+import Foundation
+import CoreData
+
+public enum UserAttributes: String 
+{
+    case email
+    case identifier
+    case majors
+    case minors
+    case name
+    case organizations
+}
+
+public class _User: NSManagedObject 
+{
+
+    // MARK: - Properties
+
+    @NSManaged public var email: String
+
+    @NSManaged public var identifier: Int32
+
+    @NSManaged public var majors: [String]
+
+    @NSManaged public var minors: [String]
+
+    @NSManaged public var name: String
+
+    @NSManaged public var organizations: [String]
+
+    // MARK: - Relationships
+
+}
+
